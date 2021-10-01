b6e051888c73d805c8c0a3d298b704c39d371bc2
Shivam Garg <shivanshagarwal2020@gmail.com>
2021-10-01 10:01:08 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".sidemenu-toggler").click(function(){
+        $(".sidemenu").addClass("active");
+    });
+    $(".close").click(function(){
+        $(".sidemenu").removeClass("active");
+    });
+
+   $(window).scroll(function(){
+        var sc = $(window).scrollTop();
+        if (sc > 150){
+            $(".header").addClass("sticky");
+        }else{
+            $(".header").removeClass("stidky");
+        }
+   }); 
+   });
